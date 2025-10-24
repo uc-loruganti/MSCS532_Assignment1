6b1ca5114c813355a7db0e1dffbbf72e2a8c0679
Laxmi Kanth Oruganti <loruganti38274@ucumberlands.edu>
2025-10-24 14:37:24 -0500

    insertion sort logic implementation

--- insertion-sort.py
+++ insertion-sort.py
@@ -3,10 +3,18 @@ import argparse
 
 def insertion_sort(arr, length):
     print("Sorting array using Insertion Sort...")
-    return arr  
     
-    
+    for index in range(1, length):
+        # Select the element to be positioned
+        key = arr[index]
 
+        prevIndex = index - 1
+        # Ascending order : sort the element into its correct position 
+        while prevIndex >= 0 and key > arr[prevIndex]:
+            arr[prevIndex + 1] = arr[prevIndex]
+            prevIndex = prevIndex -1
+        arr[prevIndex + 1] = key
+    return arr
 
 
 if __name__ == "__main__":

6b01d1b485f73b41d29f02ba3b0bf7ddede18871
Laxmi Kanth Oruganti <loruganti38274@ucumberlands.edu>
2025-10-24 14:22:12 -0500

    Basic skeleton added

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import argparse
+
+
+def insertion_sort(arr, length):
+    print("Sorting array using Insertion Sort...")
+    print("Original array:", arr)
+    
+
+
+
+if __name__ == "__main__":
+    print("Insertion Sort Implementation")
+    parser = argparse.ArgumentParser(description="Insertion Sort Algorithm")
+    parser.add_argument("--elements", nargs="*", type=int, help="List of integers to sort")
+    args = parser.parse_args()
+    elements = args.elements
+    insertion_sort(elements, len(elements))
